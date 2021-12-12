1d4bd0cc05f3920139c4109a12c2f286fe7121c7
Aleksandr Starkov <77936849+AleksandrStark@users.noreply.github.com>
2021-12-12 21:45:16 +0300

    commit

---
+++ src/components/Clock.js
@@ -0,0 +1,46 @@
+export default function Clock(props) {
+    let today = new Date();
+    let hoursStyle, minutesStyle, secondsStyle;
+    const rotation = (h, m, s) => {
+        hoursStyle = {
+            transform: `rotate(${h}deg)`
+        };
+        minutesStyle = {
+            transform: `rotate(${m}deg)`
+        }
+        secondsStyle = {
+            transform: `rotate(${s}deg)`
+        }
+    };
+
+    let h = ((today.getHours() + (+props.hour)) % 12) + today.getMinutes() / 59;
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+
+    h *= 30;
+    m *= 6;
+    s *= 6;
+
+    rotation(h, m, s);
+
+    return (
+        <div className="clocks-elem">
+            <div className="clocks-top">
+                <p className="capital">{props.title}</p>
+                <button className="form-btn btn-delete" onClick={() => props.deleteClock(props.id)}>✕</button>
+            </div>
+            <div className="clock">
+                <div className="hand hours" style={hoursStyle}></div>
+                <div className="hand minutes" style={minutesStyle}></div>
+                <div className="hand seconds" style={secondsStyle}></div>
+                <div className="point"></div>
+                <div className="marker">
+                    <span className="marker__1"></span>
+                    <span className="marker__2"></span>
+                    <span className="marker__3"></span>
+                    <span className="marker__4"></span>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/WorldClock.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { nanoid } from 'nanoid';
+import WorldClockModel from './WorldClockModel';
+import Clock from './Clock';
+
+class WorldClock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: new Date(),
+            title: '',
+            hour: '',
+            clocks: []
+        }
+    }
+
+    handleDataChange = (evt) => {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        })
+    }
+
+    handleSubmit = (evt) => {
+        evt.preventDefault();
+        const clocks = new WorldClockModel(nanoid(), this.state.title, this.state.hour);
+        this.setState({
+            clocks: [...this.state.clocks, clocks],
+            title: '',
+            hour: ''
+        })
+    }
+
+    componentDidMount() {
+        this.time = setInterval(() => {
+            this.setState({
+                time: new Date(Date.now() + this.state.hour * 60 * 60 * 1000).toLocaleTimeString()
+            })
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.time);
+    }
+
+    deleteClock = (id) => {
+        this.setState({
+            clocks: this.state.clocks.filter(clock => clock.id !== id)
+        });
+    }
+
+    render() {
+        return (
+            <>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        name="title"
+                        value={this.state.title}
+                        onChange={this.handleDataChange}
+                        placeholder="Название столицы"
+                        className="form-clock" />
+                    <input
+                        type="number"
+                        name="hour"
+                        value={this.state.hour}
+                        onChange={this.handleDataChange}
+                        placeholder="Временная зона"
+                        className="form-clock" />
+                    <button type="submit" className="form-btn">Добавить</button>
+                </form>
+                <div className="clocks">
+                    {this.state.clocks.map((element) =>
+                        <Clock
+                            key={nanoid()}
+                            id={element.id}
+                            title={element.title}
+                            hour={element.hour}
+                            deleteClock={this.deleteClock}
+                        />)
+                    }
+                </div>
+            </>
+        )
+    }
+}
+
+export default WorldClock
